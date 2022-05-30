a2840b5589e40412e436ce4efd019bb35e393df4
cronfy <cronfy@gmail.com>
2022-05-30 15:57:29 +0400

    uodate

---
+++ bitrix/initial-www/index.php
@@ -0,0 +1,7 @@
+<h1>Initial</h1>
+
+<ul>
+  <li><a href="./short-tag.php">Short tag test</a></li>
+  <li><a href="./phpinfo.php">phpinfo</a></li>
+</ul>
+
---
+++ bitrix/initial-www/short-tag.php
@@ -0,0 +1,13 @@
+<?
+
+ob_start();
+
+?>
+FAILED: if you see this, short tag does not work.
+<?
+
+ob_end_clean();
+
+echo "PASSED: Short tag works";
+
+
